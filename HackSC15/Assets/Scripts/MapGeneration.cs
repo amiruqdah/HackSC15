94ef164bf78f9f55efb368e51231711960b4e1eb
Lucas Tecot <lucastecot@gmail.com>
2015-11-14 04:44:53 -0800

    top down build approach

--- HackSC15/Assets/Scripts/MapGeneration.cs
+++ HackSC15/Assets/Scripts/MapGeneration.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class MapGeneration : MonoBehaviour {
+	//TODO: Combine meshes for performance optimization
+
 	public int sizeX, sizeY, sizeZ;
+	private int[,] map;
 	// Use this for initialization
 	void Start () 
 	{
@@ -10,14 +13,17 @@ public class MapGeneration : MonoBehaviour {
 		sizeY = 10;
 		sizeZ = 10;
 
-		for(int x = 0; x < sizeX; x++)
+		map = new int[sizeX, sizeY];
+
+		for(int x = sizeX; x > 0; x--)
 		{
-			for(int y = 0; y < sizeY; y++)
+			for(int y = sizeY; y > 0; y--)
 			{
-				for(int z = 0; z < sizeZ; z++)
+
+				for(int z = sizeZ; z > 0; z--)
 				{
-					GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);'
-					temp.transform.position = new Vector3(x + 1, y + 1, z + 1);
+					GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					temp.transform.position = new Vector3(x - 1, y - 1, z - 1);
 				}
 			}
 		}
