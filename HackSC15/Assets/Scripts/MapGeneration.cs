08f49ad9e37003da947d62b30fb329223eabcb45
Lucas Tecot <lucastecot@gmail.com>
2015-11-14 04:36:40 -0800

    base map cube code

--- HackSC15/Assets/Scripts/MapGeneration.cs
+++ HackSC15/Assets/Scripts/MapGeneration.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class MapGeneration : MonoBehaviour {
 	public int sizeX, sizeY, sizeZ;
-
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		sizeX = 10;
 		sizeY = 10;
 		sizeZ = 10;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+		for(int x = 0; x < sizeX; x++)
+		{
+			for(int y = 0; y < sizeY; y++)
+			{
+				for(int z = 0; z < sizeZ; z++)
+				{
+					GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);'
+					temp.transform.position = new Vector3(x + 1, y + 1, z + 1);
+				}
+			}
+		}
+
 	}
 }
