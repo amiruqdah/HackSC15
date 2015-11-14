e145e35ef22842e36523714781b2258eece68b96
Amir Uqdah <ottoman.empire12@pacbell.net>
2015-11-14 07:24:30 -0800

    set up player event system

---
+++ HackSC15/Assets/Scripts/Player/PlayerEventManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEventManager : MonoBehaviour {
+
+	// Possible Player Events
+
+	public delegate void SpawnEvent();
+	public static event SpawnEvent onSpawn;
+	public delegate void DieEvent();
+	public static event DieEvent onDeath;
+	public delegate void MoveEvent();
+	public static event MoveEvent onMove;
+	public delegate void KeyEvent();
+	public delegate void WinEvent();
+	public static event WinEvent onWinEvent;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
