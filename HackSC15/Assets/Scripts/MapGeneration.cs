1511333205ba83f99596c8e56d9c102e02b2e47b
Lucas Tecot <lucastecot@gmail.com>
2015-11-14 04:21:27 -0800

    created basic map script

---
+++ HackSC15/Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+	public int sizeX, sizeY, sizeZ;
+
+	// Use this for initialization
+	void Start () {
+		sizeX = 10;
+		sizeY = 10;
+		sizeZ = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
