0ec2e9f25d0b0dbc6e646685b7f0dc1a8998eade
Lucas Tecot <lucastecot@gmail.com>
2015-11-14 06:25:22 -0800

    Generation built, not final version

--- HackSC15/Assets/Scripts/MapGeneration.cs
+++ HackSC15/Assets/Scripts/MapGeneration.cs
@@ -4,26 +4,67 @@ using System.Collections;
 public class MapGeneration : MonoBehaviour {
 	//TODO: Combine meshes for performance optimization
 
-	public int sizeX, sizeY, sizeZ;
+	public int size, height, maxDrop;
 	private int[,] map;
+	private int step;
 	// Use this for initialization
 	void Start () 
 	{
-		sizeX = 10;
-		sizeY = 10;
-		sizeZ = 10;
+		size = 10;
+		height = 15;
+		maxDrop = 3;
 
-		map = new int[sizeX, sizeY];
-
-		for(int x = sizeX; x > 0; x--)
+		map = new int[size, size];
+		for (int i = 0; i < size; i++) 
 		{
-			for(int y = sizeY; y > 0; y--)
+			for(int z = 0; z < size; z++)
 			{
+				map[i,z] = -1;
+			}
+		}
+		map [size - 1, size - 1] = height - 1;
 
-				for(int z = sizeZ; z > 0; z--)
+		for (step = size - 1; step > 0; step--) 
+		{
+			for(int x = size - 1; x >= step; x--)
+			{
+				double rand = Random.value;
+				map[x, step - 1] = map[x, step] - (int)(rand * maxDrop);
+				if(x == step)
+				{
+					map[x - 1, step - 1] = map[x, step] - (int)(rand * maxDrop);
+				}
+			}
+			for(int y = size - 1; y >= step; y--)
+			{
+				double rand = Random.value;
+				map[step - 1, y] = map[step, y] - (int)(rand * maxDrop);
+				if(y == step)
+				{
+					if(Random.value > 0.5)
+						map[step - 1, y - 1] = map[step, y] - (int)(rand * maxDrop);
+				}
+			}
+		}
+
+		for (int i = 0; i < size; i++) 
+		{
+			for(int z = 0; z < size; z++)
+			{
+				if(map[i,z] < 0)
+					map[i,z] = 0;
+			}
+		}
+
+		for(int x = size - 1; x >= 0; x--)
+		{
+			for(int y = size - 1; y >= 0; y--)
+			{
+				int t = map[x, y];
+				for( ;t >= 0; t--)
 				{
 					GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					temp.transform.position = new Vector3(x - 1, y - 1, z - 1);
+					temp.transform.position = new Vector3(x, t, y);
 				}
 			}
 		}
