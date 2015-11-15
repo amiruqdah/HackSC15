165ce0f67022a54b13d7ca7e24424790626829c1
Amir Uqdah <ottoman.empire12@pacbell.net>
2015-11-14 22:56:26 -0800

    added camera drift

---
+++ HackSC15/Assets/Scripts/Level/CameraDrift.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+[RequireComponent(typeof(Camera))]
+public class CameraDrift : MonoBehaviour {
+
+	private Camera cameraObj;
+	private float initialFOV;
+	public float amplitude;
+	public float speed; 
+	private float initalX;
+
+	// Use this for initialization
+	void Start () {
+		cameraObj = this.GetComponent<Camera>();
+		initialFOV = this.GetComponent<Camera>().fieldOfView;
+		initalX = this.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// create a slight varianec in their time scales and linear relations
+		cameraObj.fieldOfView = initialFOV + amplitude * Mathf.Sin(speed * Time.time);
+		this.transform.position = new Vector3(initalX + amplitude * Mathf.Cos (speed * Time.time), this.transform.position.y, this.transform.position.z);
+	}
+}
